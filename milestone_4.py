c1b58168fba479d014e3c1caa25c7828bda4ef62
Sym Bains <Sym@Syms-MacBook-Pro.local>
2024-10-24 13:11:06 +0100

    Refactoring and code optimisation

--- milestone_4.py
+++ milestone_4.py
@@ -1,25 +1,36 @@
 import random
 
+word_list = ['apple', 'banana', 'kiwi', 'orange', 'melon']
+
 class Hangman:
     def __init__(self, word_list, num_lives=5):
-        self.word_list = ['apple', 'banana', 'kiwi', 'orange', 'melon']
+        """Initialize the attributes for the Hangman game."""
+        self.word_list = word_list  
         self.word = random.choice(self.word_list)
         self.word_guessed = ['_' for _ in self.word]
         self.num_letters = len(set(self.word))
         self.num_lives = num_lives
         self.list_of_guesses = []
 
-    def check_guess(self, guess):
+    def _update_word_guessed(self, guess):
+        """Update the displayed word with the guessed letter."""
+        for index, letter in enumerate(self.word):
+            if letter == guess:
+                self.word_guessed[index] = guess 
+
+    def _is_guess_correct(self, guess):
         """Check if the guessed letter is in the word."""
-        guess = guess.lower() 
-        if guess in self.word:
+        return guess in self.word
+
+    def check_guess(self, guess):
+        """Evaluate the guessed letter and update game state accordingly."""
+        guess = guess.lower()
+        if self._is_guess_correct(guess):
             print(f"Good guess! '{guess}' is in the word.")
-            for index, letter in enumerate(self.word):
-                if letter == guess:
-                    self.word_guessed[index] = guess  
-            self.num_letters -= 1  
+            self._update_word_guessed(guess) 
+            self.num_letters -= 1
         else:
-            self.num_lives -= 1  # Reduce the number of lives
+            self.num_lives -= 1
             print(f"Sorry, '{guess}' is not in the word.")
             print(f"You have {self.num_lives} lives left.")
 
@@ -33,8 +44,7 @@ class Hangman:
             elif guess in self.list_of_guesses:
                 print("You already tried that letter!")
             else:
-                self.check_guess(guess)  
                 self.list_of_guesses.append(guess)  
+                self.check_guess(guess)  
                 break  
 
-ask_for_input()
